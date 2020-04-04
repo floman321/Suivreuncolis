3586a14a25feff99a569f38b23320b3762331db1
Yoan <yoan-m@users.noreply.github.com>
2020-04-04 23:20:34 +0200

    Migration v4

--- desktop/php/Suivreuncolis.php
+++ desktop/php/Suivreuncolis.php
@@ -1,101 +1,98 @@
 <?php
-    if (!isConnect('admin')) {
-        throw new Exception('{{401 - Accès non autorisé}}');
-    }
-    sendVarToJS('eqType', 'Suivreuncolis');
-    $eqLogics = eqLogic::byType('Suivreuncolis');
-    ?>
+if (!isConnect('admin')) {
+	throw new Exception('{{401 - Accès non autorisé}}');
+}
+$plugin = plugin::byId('Suivreuncolis');
+sendVarToJS('eqType', $plugin->getId());
+$eqLogics = eqLogic::byType($plugin->getId());
+?>
 
 <div class="row row-overflow">
-<div class="col-lg-2 col-md-3 col-sm-4">
-<div class="bs-sidebar">
-<ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
-<a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un Suivi}}</a>
-<li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-<?php
-    foreach ($eqLogics as $eqLogic) {
-        echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
-    }
-    ?>
-</ul>
-</div>
-</div>
-<div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-<legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
+   <div class="col-xs-12 eqLogicThumbnailDisplay">
+  <legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
+  <div class="eqLogicThumbnailContainer">
+      <div class="cursor eqLogicAction logoPrimary" data-action="add">
+        <i class="fas fa-plus-circle"></i>
+        <br>
+        <span>{{Ajouter}}</span>
+    </div>
+      <div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
+      <i class="fas fa-wrench"></i>
+    <br>
+    <span>{{Configuration}}</span>
+  </div>
+  </div>
+  <legend><i class="icon divers-mailbox15"></i> Mes Suivis</legend>
+	   <input class="form-control" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
 <div class="eqLogicThumbnailContainer">
-<div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-<center>
-<i class="fa fa-plus-circle" style="font-size : 5em;color:#00979c;"></i>
-</center>
-<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#00979c"><center>Ajouter</center></span>
+    <?php
+foreach ($eqLogics as $eqLogic) {
+	$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+	echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
+	echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+	echo '<br>';
+	echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+	echo '</div>';
+}
+?>
+</div>
 </div>
 
-<div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
-<center>
-<i class="fa fa-wrench" style="font-size : 5em;color:#767676;"></i>
-</center>
-<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
-</div>
-</div>
-</div>
-<div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-<legend><i class="icon divers-mailbox15"></i> Mes Suivis</legend>
-
-<div class="eqLogicThumbnailContainer">
-<?php
-    foreach ($eqLogics as $eqLogic) {
-        echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
-        echo "<center>";
-        echo '<img src="plugins/Suivreuncolis/doc/images/Suivreuncolis_icon.png" height="105" width="95" />';
-        echo "</center>";
-        echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
-        echo '</div>';
-    }
-    ?>
-</div>
-</div>
-<div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-<a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-<a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-<a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
-<ul class="nav nav-tabs" role="tablist">
-<li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
-<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
-<li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
-</ul>
-<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
-<div role="tabpanel" class="tab-pane active" id="eqlogictab">
-<br/>
-
-<form class="form-horizontal">
-<fieldset>
-<div class="form-group">
-<label class="col-sm-3 control-label">{{Nom du colis}}</label>
-<div class="col-sm-3">
-<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom du colis}}"/>
-</div>
-</div>
-<div class="form-group">
-<label class="col-sm-3 control-label" >{{Objet parent}}</label>
-<div class="col-sm-3">
-<select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-<option value="">{{Aucun}}</option>
-<?php
-    foreach (jeeObject::all() as $object) {
-        echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
-    }
-    ?>
-</select>
-</div>
-</div>
-<div class="form-group">
-<label class="col-sm-3 control-label" >{{Activer}}</label>
-<div class="col-sm-9">
-<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
-</div>
-</div>
+<div class="col-xs-12 eqLogic" style="display: none;">
+		<div class="input-group pull-right" style="display:inline-flex">
+			<span class="input-group-btn">
+				<a class="btn btn-default btn-sm eqLogicAction roundedLeft" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a><a class="btn btn-default btn-sm eqLogicAction" data-action="copy"><i class="fas fa-copy"></i> {{Dupliquer}}</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
+			</span>
+		</div>
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
+    <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
+    <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+  </ul>
+  <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+    <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+      <br/>
+    <form class="form-horizontal">
+        <fieldset>
+            <div class="form-group">
+                <label class="col-sm-3 control-label">{{Nom de l'équipement template}}</label>
+                <div class="col-sm-3">
+                    <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                    <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement template}}"/>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+                <div class="col-sm-3">
+                    <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+                        <option value="">{{Aucun}}</option>
+                        <?php
+foreach (jeeObject::all() as $object) {
+	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+}
+?>
+                   </select>
+               </div>
+           </div>
+	   <div class="form-group">
+                <label class="col-sm-3 control-label">{{Catégorie}}</label>
+                <div class="col-sm-9">
+                 <?php
+                    foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+                    echo '<label class="checkbox-inline">';
+                    echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+                    echo '</label>';
+                    }
+                  ?>
+               </div>
+           </div>
+	<div class="form-group">
+		<label class="col-sm-3 control-label"></label>
+		<div class="col-sm-9">
+			<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+			<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+		</div>
+	</div>
 <div class="form-group">
 <label class="col-sm-3 control-label">Numéro Suivi</label>
 <div class="col-sm-3">
